245f06bbbe7fbf7e8fbf34ed40edfffa6e6cec16
merafiulhoque <me.rafiulhoque@gmail.com>
2025-12-17 09:49:55 +0530

    new commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/connectDB";
+
+dotenv.config({
+    path: "./env"
+})
+
+connectDB()
+.then(()=>{
+    console.log("Connected Successfully");
+    app.listen(process.env.PORT || 5000 , () => {
+        console.log(`Server is running at PORT ${process.env.PORT? process.env.PORT : 5000}`);
+    })
+})
+.catch((error)=>{
+    console.log(`Error connecting Database`,error)
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+// const asyncHandler = (fn) =>async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
+
+const asyncHandler = async (fn) => {
+    (req,res,next) => {
+        Promise.resolve(req,res,next).catch(err => next(err))
+    }
+}
+
+
+
+
+export {asyncHandler}
